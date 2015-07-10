42b078858439cd8dac385178747e4166bfdcda6a
Jake Ohara <jakeohara12@gmail.com>
2015-07-09 18:45:11 -0700

    solved problem 5

---
+++ src/problem005.py
@@ -0,0 +1,18 @@
+#problem 5
+
+
+list = [11,13,14,16,17,18,19,20]
+
+def solution(step):
+   for num in range(step, 999999999, step):
+      if all(num % n == 0 for n in list):
+         return num 
+      return None 
+  
+if __name__ == '__main__':
+   solution = solution(20)
+   if solution is None:    
+      print("no ans")
+   else:
+      print(solution)
+      
