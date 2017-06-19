b8f79305da2529ead784b6100b24b32ea7043615
Jake Ohara <jakeohara12@gmail.com>
2017-06-18 22:27:01 -0700

    Add solutions 11-50

---
+++ problem024.py
@@ -0,0 +1,8 @@
+def compute():
+	arr = list(range(10))
+	temp = itertools.islice(itertools.permutations(arr), 999999, None)
+	return "".join(str(x) for x in next(temp))
+
+
+if __name__ == "__main__":
+	print(compute())
